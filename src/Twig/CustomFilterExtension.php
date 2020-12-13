400c53ecf8c8ff8f7291cb9a504b7a4f796d39f9
ThomasBerranger <tberranger@hotmail.fr>
2020-12-13 20:21:46 +0100

    cards design updated

---
+++ src/Twig/CustomFilterExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class CustomFilterExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('pluralize', [$this, 'pluralize']),
+            new TwigFilter('textLimit', [$this, 'textLimit']),
+        ];
+    }
+
+    public function pluralize($value): string
+    {
+        if ($value and is_iterable($value)) {
+            return 's';
+        } else {
+            return '';
+        }
+    }
+
+    public function textLimit($text, $charactersNumber): string
+    {
+        if (strlen($text) >= $charactersNumber) {
+            return mb_substr($text, 0, $charactersNumber).' ...';
+        } else {
+            return $text;
+        }
+    }
+}
