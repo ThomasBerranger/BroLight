207979afe9fdd5a6bb271c2253ed503433ddb478
ThomasBerranger <tberranger@hotmail.fr>
2020-12-24 12:42:49 +0100

    admin users list

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +12,15 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AdminController extends AbstractController
 {
-
     /**
      * @Route("", name="home")
      */
-    public function index(): Response
+    public function home(): Response
     {
-        return $this->render('admin/index.html.twig');
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        return $this->render('admin/home.html.twig', [
+            'users' => $users
+        ]);
     }
 }
