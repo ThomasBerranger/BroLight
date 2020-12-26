a71bb9769ca4a8fcfdb293cd0b25542e56a96dc5
ThomasBerranger <tberranger@hotmail.fr>
2020-12-26 16:16:52 +0100

    sidebar updated

--- assets/js/components/sidebar.js
+++ assets/js/components/sidebar.js
@@ -1,12 +1,8 @@
 import $ from 'jquery';
 
-$(document).ready(function () {
-    $('.btn-header-sidebar').click(function () {
-        const sidebar = $('#sidebar');
-        if(sidebar.css('display') === 'none') {
-            sidebar.css({"margin-left": 0, "display": "block"});
-        } else {
-            sidebar.css({"margin-left": '100vw', "display": "none"});
-        }
+$(function(){
+    $(".btn-header-sidebar").on("click", function(){
+        $("#sidebar").toggleClass("open");
+        $("body").toggleClass("overflow-hidden");
     });
 });
