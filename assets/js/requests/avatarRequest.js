3cc9d0b91c150ea778bfc97f8e366293089a2df2
ThomasBerranger <tberranger@hotmail.fr>
2021-01-30 15:10:12 +0100

    rewording

--- assets/js/requests/avatarRequest.js
+++ assets/js/requests/avatarRequest.js
@@ -10,6 +10,10 @@ $(document).ready(function() {
             data: $(this).serializePrefixedFormJSON(),
             dataType: "json",
             success: function () {
+                const avatarHeader = $('.avatar-header');
+                if (avatarHeader.length === 1) {
+                    avatarHeader.parent().load(' .avatar-header');
+                }
                 $('#alert').trigger("trigger-alert", ["success", "Avatar sauvegardé !"]);
             },
             error: function (error) {
