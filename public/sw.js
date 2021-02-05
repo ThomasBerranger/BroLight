ed4ee93b4cec3bf1ba6c34af3da7f190a544f081
DESKTOP-D0F3KCP\tberr <tberranger@hotmail.fr>
2021-02-05 15:21:17 +0100

    no pwa cache

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,22 @@
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open('pwa').then(cache => {
-            return cache.addAll([
-                '/',
-                '/movie/trending',
-                '/sw.js',
-            ])
-                .then(() => self.skipWaiting());
-        })
-    )
-});
-
-self.addEventListener('activate',  event => {
-    event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
+// self.addEventListener('install', e => {
+//     e.waitUntil(
+//         caches.open('pwa').then(cache => {
+//             return cache.addAll([
+//                 '/sw.js',
+//             ])
+//                 .then(() => self.skipWaiting());
+//         })
+//     )
+// });
+//
+// self.addEventListener('activate',  event => {
+//     event.waitUntil(self.clients.claim());
+// });
+//
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//         caches.match(event.request).then(response => {
+//             return response || fetch(event.request);
+//         })
+//     );
+// });
