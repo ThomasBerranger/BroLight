e6f53707f6ac425b4830d5ac946c84387ccc3437
DESKTOP-D0F3KCP\tberr <tberranger@hotmail.fr>
2021-03-03 16:12:36 +0100

    trending page in progress

---
+++ src/Service/MovieService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Service;
+
+class MovieService
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function getCorrespondingGenres(int $id): array
+    {
+        return [];
+    }
+
+}
