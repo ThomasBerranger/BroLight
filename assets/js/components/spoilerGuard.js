9b298da4382976b9f3cd30eae8a7a912980c318c
DESKTOP-D0F3KCP\tberr <tberranger@hotmail.fr>
2021-03-21 18:41:52 +0100

    spoiler guard ready

--- assets/js/components/spoilerGuard.js
+++ assets/js/components/spoilerGuard.js
@@ -3,7 +3,11 @@ import $ from "jquery";
 $(function() {
    $('body').on('click', '.spoiler-guard',function() {
       $(this).fadeOut(200, function () {
-         $(this).next().fadeIn(200);
+         $(this).prev().css({
+            'transition' : '2s',
+            'text-shadow' : 'none',
+            'color' : 'black'
+         });
       });
    });
 })
