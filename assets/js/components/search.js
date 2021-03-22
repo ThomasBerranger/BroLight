4e88f5a6ad424aaa0ddc358d76eaf7bb238698a9
DESKTOP-D0F3KCP\tberr <tberranger@hotmail.fr>
2021-03-22 15:09:48 +0100

    search front up

--- assets/js/components/search.js
+++ assets/js/components/search.js
@@ -2,32 +2,45 @@ import $ from "jquery";
 import { debounce } from '../functions/debounce';
 
 $(document).ready(function(){
+    const headerSearchInput = $("#headerSearchInput");
     const searchDiv = $(".search");
+    const icon = $("nav > i");
 
-    $("#headerSearchInput").on('keyup', debounce(function(){
+    headerSearchInput.on('keyup', debounce(function(){
+        const url = $(this).data('url');
         const text = $(this).val();
-        const url = $(this).data('url')
 
         if(text.length > 2) {
             $.ajax({
                 url: url.replace("replaceMe", text),
                 success: function(view) {
+
+                    $('.container').css('position', 'fixed');
                     $("#searchResultDiv").html(view);
-                    searchDiv.css('height', '100vh');
-                    searchDiv.css('background-color', 'rgba(0, 0, 0, 0.6');
+                    icon.removeClass('fa-search');
+                    icon.addClass('fa-times pointer-cursor');
+                    searchDiv.css({'height': '100vh'});
+
                 }
             });
         } else {
+            $('.container').css('position', 'static');
             $('#searchResultDiv').children().remove();
+            icon.addClass('fa-search');
+            icon.removeClass('fa-times pointer-cursor');
             searchDiv.css('height', 0);
-            searchDiv.css('background-color', 'rgba(0, 0, 0, 0.6');
         }
 
     }, 350))
 
-    $("nav > img").click(function () {
-        $('#searchResultDiv').children().remove();
-        searchDiv.css('height', 0);
-        searchDiv.css('background-color', 'rgba(0, 0, 0, 0.6');
+    icon.click(function () {
+        if (headerSearchInput.val().length > 2) {
+            $('.container').css('position', 'static');
+            $('#searchResultDiv').children().remove();
+            $(this).addClass('fa-search');
+            $(this).removeClass('fa-times pointer-cursor');
+            searchDiv.css('height', 0);
+            headerSearchInput.val('');
+        }
     });
 });
